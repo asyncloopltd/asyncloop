408574ebc6edde174f08129babdfe90bb37e5b8e
Michel uncini <uncini.michel@gmail.com>
2019-12-19 22:40:32 +0000

    init

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import logo from '../assets/images/logo.svg';
+
+const Header = (props) => (
+    <header id="header" className="alt">
+        <span className="logo"><img src={logo} alt=""/></span>
+        <h1>AsyncLoop</h1>
+        <p>Obsessed to delivery value.<br/>
+            Sometimes for free.</p>
+    </header>
+)
+
+export default Header
